074e439f6fd65e448cc00db298fb4847c84f57b7
bag2649 <bag2649@hotmail.com>
2023-06-09 15:41:54 -0400

    Supress files / New start
    We had merging errors so were deleting the main to restart clean and push all ours dev_branch on it.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Pokedex
-TP2 concernant la création d'un pokédex en équipe de quatre.
--- pokedex/src/app/pokemon/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-export default function PokemonDetails() {
-  return (
-    <>pokemon</>
-  )}
